5146d421be429d011dd6e4966af1573453404667
vitorbruski <vitorgbruski@gmail.com>
2023-10-02 02:51:43 -0300

    [container]: adicionado export

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export { default as Button } from "./components/button"
 export { default as Card } from "./components/card"
+export { default as Container } from "./components/container"
 export { default as Header } from "./components/header"
 export { default as Icon } from "./components/icon"
 export { default as Input } from "./components/input"
