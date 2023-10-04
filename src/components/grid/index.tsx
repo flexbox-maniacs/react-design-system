13136626a87821f5c86abc08f5e970093c2031af
vitorbruski <vitorgbruski@gmail.com>
2023-10-04 18:28:51 -0300

    gr

--- src/components/grid/index.tsx
+++ src/components/grid/index.tsx
@@ -2,8 +2,8 @@ import { useClassName } from "@scripts"
 import { GridTypes, GridProps } from "./types"
 import "./style.scss"
 
-function Grid({ grid = {}, children }: GridTypes): JSX.Element {
-  const { alignment = "center" }: GridProps = grid
+function Grid({ grid, children }: GridTypes): JSX.Element {
+  const { alignment = "center" }: GridProps = grid || {}
   const gridClassList = {
     alignment: `align-${alignment}`,
   }
